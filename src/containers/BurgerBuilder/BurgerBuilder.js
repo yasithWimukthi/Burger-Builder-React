bcc994c88c63188f41b0245680cd074eddb3f9c7
yasithWimukthi <it19966922@my.sliit.lk>
2020-11-09 21:44:37 +0530

    Edit Burger.js

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Auxi from '../../hoc/Auxi';
+import Burger from '../../components/Burger/Burger';
 
 
 export default class BurgerBuilder extends Component {
@@ -8,7 +9,7 @@ export default class BurgerBuilder extends Component {
     render() {
         return (
             <Auxi>
-                <div>Burger</div>
+                <Burger />
                 <div>Build control area</div>
             </Auxi>
         )
