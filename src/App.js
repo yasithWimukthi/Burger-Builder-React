5039d61465c021ad9a3969acbead14839fa147ff
yasithWimukthi <it19966922@my.sliit.lk>
2020-11-09 18:03:20 +0530

    create BurgerBuilder.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ class App extends Component {
   render() {
     return (
       <div >
-
+        <Layout>
+          <p>Test</p>
+        </Layout>
       </div>
     );
   }
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Auxi from '../../hoc/Auxi';
 
-export default function Layout() {
+export default function Layout(props) {
     return (
+        <Auxi>
         <div>
-            
+            toolbar
         </div>
+
+        <main>
+            {props.children}
+        </main>
+        </Auxi>
+
     )
 }
