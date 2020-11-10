b40d78ecf62f60df24339025059d0067786e29d5
yasithWimukthi <it19966922@my.sliit.lk>
2020-11-10 17:29:07 +0530

    Edit Burger.js

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,16 +4,28 @@ import classes from './Burger.css';
 
 const Burger = (props) => {
 
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
             .map(igKey =>{
                 return [...Array(props.ingredients[igKey])].map((_,i)=>{
                     return <BurgerIngredient key={igKey + 1} type={igKey}/>
                 });
-            });
+            }).reduce((arr,el)=>{
+                return arr.concat(el);
+            },[])
+    
     /**
      *  Object.keys() method returns an array of a given object's own enumerable property names, 
      * iterated in the same order that a normal loop would
      */
+
+     if(transformedIngredients.length === 0){
+        /**
+         * If user does not select ingredients then an error message wil be displayed
+         */
+        transformedIngredients = <p>Please add ingredients!!</p>
+     }
+
+
     return (
         <div className={classes.burger}>
             <BurgerIngredient type="bread-top"/>
