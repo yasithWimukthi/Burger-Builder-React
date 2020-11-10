12bb0da36ddc42d22239597fef7a39ddb5494e31
yasithWimukthi <it19966922@my.sliit.lk>
2020-11-10 16:50:53 +0530

    add states to BurgerBuilder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,11 +5,19 @@ import Burger from '../../components/Burger/Burger';
 
 export default class BurgerBuilder extends Component {
 
+    state = {
+        ingredients : {
+            salad: 1,
+            bacon: 1,
+            chesse: 2,
+            meat: 2
+        }
+    }
 
     render() {
         return (
             <Auxi>
-                <Burger />
+                <Burger ingredients={this.state.ingredients} />
                 <div>Build control area</div>
             </Auxi>
         )
