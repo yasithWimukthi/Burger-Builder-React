97fc9788f05d5e2701a9fad55bc6934a528fe33e
yasithWimukthi <it19966922@my.sliit.lk>
2020-11-11 18:44:33 +0530

    Implement addIngredientHandler method

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,26 +1,49 @@
 import React, { Component } from 'react';
 import Auxi from '../../hoc/Auxi';
 import Burger from '../../components/Burger/Burger';
-import BuildControls from '../../components/Burger/BuildControls/BuildControls'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+    salad : 0.5,
+    cheese : 0.4,
+    meat : 1.3,
+    bacon : 0.7
+}
 
 export default class BurgerBuilder extends Component {
 
     state = {
         ingredients : {
-            salad: 1,
-            bacon: 1,
-            cheese: 2,
-            meat: 2
-        }
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        },
+        totalPrice : 4
     }
 
     addIngredientHandler = type =>{
+        // SET INGREDIENT COUNT
+        const OLD_COUNT =  this.state.ingredients[type];
+        const UPDATE_COUNT = OLD_COUNT + 1;
+        const UPDATE_INGREDIENTS = {
+            ...this.state.ingredients
+        }
+        UPDATE_INGREDIENTS[type] = UPDATE_COUNT;
 
+        // SET TOTAL PRICE
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+
+        this.setState({
+            ingredients : UPDATE_INGREDIENTS,
+            totalPrice : newPrice
+        });
     }
 
     removeIngredientHandler = type =>{
-        
+
     }
 
     render() {
