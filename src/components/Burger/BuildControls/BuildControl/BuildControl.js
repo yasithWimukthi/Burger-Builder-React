605d34b6965ec6441e9a828ea77759d655329f08
yasithWimukthi <it19966922@my.sliit.lk>
2020-11-12 22:57:28 +0530

    Edit BuildControl.js

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,8 +5,14 @@ function BuildControl(props) {
     return (
         <div className={classes.BuildControl}>
             <div className={classes.Label}>{props.label}</div>
-            <button className={classes.Less} onClick={props.removed}>Less</button>
-            <button className={classes.More} onClick={props.added}>More</button>
+            <button 
+                className={classes.Less} 
+                onClick={props.removed} 
+                disabled={props.disabled}>Less</button>
+
+            <button c
+                lassName={classes.More} 
+                onClick={props.added}>More</button>
         </div>
     )
 }
