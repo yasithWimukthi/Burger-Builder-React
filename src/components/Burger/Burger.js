dba390c3eac0e3b474e90b85100883309b80fc65
yasithWimukthi <it19966922@my.sliit.lk>
2020-11-12 23:12:51 +0530

    Edit Burger.js

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,12 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.css';
 
 const Burger = (props) => {
-
+    let count = 0;
     let transformedIngredients = Object.keys(props.ingredients)
             .map(igKey =>{
                 return [...Array(props.ingredients[igKey])].map((_,i)=>{
-                    return <BurgerIngredient key={igKey + 1} type={igKey}/>
+                    count++;
+                    return <BurgerIngredient key={igKey + count} type={igKey}/>
                 });
             }).reduce((arr,el)=>{
                 return arr.concat(el);
