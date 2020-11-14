65f219344169e2aa9c90d0c7f25490b68b168bb3
yasithWimukthi <it19966922@my.sliit.lk>
2020-11-14 18:14:52 +0530

    Edit Modal.js

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -2,11 +2,18 @@ import React from 'react';
 import classes from './Modal.css';
 
 function Modal(props) {
+
+    const styles = {
+        transform : props.show ? 'translateY(0)' : 'translateY(-100vh)' ,
+        opacity : props.show ? '1' : '0'
+    }
+    
     return (
-        <div className={classes.Modal}>
+        <div className={classes.Modal} style={styles}>
             {props.children}
         </div>
     )
 }
+
 
 export default Modal;
