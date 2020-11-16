23696f162c78c9957902316292e8e81020536f36
yasithWimukthi <it19966922@my.sliit.lk>
2020-11-16 21:12:42 +0530

    Implement Toolbar component

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import classes from './Toolbar.css';
 
 const Toolbar = (props) => {
     return (
-        <header>
-            
+        <header className={classes.toolbar}>
+          <div>menu</div>
+          <div>logo</div>  
+          <nav>
+              ....
+          </nav>
         </header>
     )
 }
