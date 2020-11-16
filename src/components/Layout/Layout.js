2c2507b6a3118a5c4ff4b506df0afd6320752dbd
yasithWimukthi <it19966922@my.sliit.lk>
2020-11-16 21:16:53 +0530

    Edit Layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import classes from './Layout.css';
 import Auxi from '../../hoc/Auxi';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 export default function Layout(props) {
     return (
         <Auxi>
-        <div>
-            toolbar
-        </div>
+        <Toolbar />
 
         <main className={classes.content}>
             {props.children}
