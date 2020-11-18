d2b8c9202a26b11ac7c9835cc72cea459600e171
yasithWimukthi <it19966922@my.sliit.lk>
2020-11-18 21:21:13 +0530

    Edit Layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React,{Component} from 'react';
 import classes from './Layout.css';
 import Auxi from '../../hoc/Auxi';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-export default function Layout(props) {
-    return (
-        <Auxi>
-            <Toolbar />
-            <SideDrawer />
-            <main className={classes.content}>
-                {props.children}
-            </main>
-        </Auxi>
+class Layout extends Component{
 
-    )
+    state = {
+        showSideDrawer : true
+    }
+
+    sideDrawerCloseHandler =  ()=>{
+        this.setState = {
+            showSideDrawer : false
+        }
+    }
+
+    render(){
+        return (
+            <Auxi>
+                <Toolbar />
+                <SideDrawer closed={this.sideDrawerCloseHandler} />
+                <main className={classes.content}>
+                    {this.props.children}
+                </main>
+            </Auxi>
+        )
+    }
 }
+
+export default Layout;
